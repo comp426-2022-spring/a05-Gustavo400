8a0fad33ffeed7a64e82f7eafbc593b122203639
Gustavo400 <Gustavo4@live.unc.edu>
2022-04-29 12:28:44 -0400

    setting up images

--- public/main.js
+++ public/main.js
@@ -8,5 +8,10 @@
 // Guess a flip by clicking either heads or tails button
 
 function showPage(pageID) {
-    alert(pageID);
+    // alert(pageID);
+    const pages = document.querySelectorAll(".page");
+    const activePage = document.querySelector(`#${pageID}`);
+    pages.forEach((page) => { page.classList.add("hidden") });
+    activePage.classList.remove("hidden");
+    // console.log(pages);
 }
